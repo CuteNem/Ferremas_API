2617ea6b231c8c6ad8e77dfb3ec3ad08564b128e
Nemesis <107058519+CuteNem@users.noreply.github.com>
2024-05-29 23:29:34 -0400

    API pt_1

--- app/categoria.py
+++ app/categoria.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
-    return jsonify({'Mensaje':'Bienvenido'})
+    return jsonify({'Mensaje':'Bienvenido, hola! prueba11'})
 
 if __name__=="__main__":
     app.run(debug=True)

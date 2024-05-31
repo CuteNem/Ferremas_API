134842766ac357ff3c9da8ad975a574289aeb651
Nemesis <107058519+CuteNem@users.noreply.github.com>
2024-05-30 20:18:26 -0400

    API pt_3

--- app/categoria.py
+++ app/categoria.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:neozorwave010103@localhost:3307/bdpythonapi'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:rootpass123@localhost:3307/bdpythonapi'
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS']= False
 
 db= SQLAlchemy(app)
 ma= Marshmallow(app)
 
+#Creación de tabla categoria
 class Categoria(db.Model):
     cat_id = db.Column(db.Integer,primary_key=True)
     cat_nom = db.Column(db.String(100))
@@ -21,6 +22,23 @@ class Categoria(db.Model):
 # Crear las tablas dentro del contexto de la aplicación
 with app.app_context():
     db.create_all()
+
+#Esquema categoria
+class CategoriaSchema(ma.Schema):
+    class Meta:
+        fields = ('cat_id','cat_nom','cat_desp')
+
+#Una sola respuesta
+categoria_schema = CategoriaSchema()
+#Cuando sean muchas respuestas
+categorias_schema = CategoriaSchema(many=True)
+
+#GET
+@app.route('/categoria',methods=['GET'])
+def get_categorias():
+    all_categorias = Categoria.query.all()
+    result = categorias_schema.dump(all_categorias)
+    return jsonify(result)
 
 #Mensaje de bienvenida
 @app.route('/',methods=['GET'])

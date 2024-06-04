594478811697a8ec73387453dbac221edeb51e9c
Nemesis <107058519+CuteNem@users.noreply.github.com>
2024-06-03 22:42:59 -0400

    NEW_API (obtener producto por id de categoria)

---
+++ app_2/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rootpass123@localhost:3307/bdtestpythonapi'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
---
+++ app_2/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    products = db.relationship('Product', backref='category', lazy=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)

2e09431fc1c5b68dd91ce359b15f2d1639a8f1ab
Benny20s <ben.sepulvedat@duocuc.cl>
2024-06-03 22:45:17 -0400

    idk

--- app/categoria.py
+++ app/categoria.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:neozorwave010103@localhost:3307/bdpythonapi'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:SE212511374@localhost:3307/bdpythonapi'
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS']= False
 
 db= SQLAlchemy(app)

f519ba76052b0c3e9ad15bc7db9756e8ba6a3a4d
Benny20s <ben.sepulvedat@duocuc.cl>
2024-06-04 13:05:07 -0400

    manteneri mi vambio

--- app_2/config.py
+++ app_2/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rootpass123@localhost:3307/bdtestpythonapi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:SE212511374@localhost:3307/bdtestpythonapi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
